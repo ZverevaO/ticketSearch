be8789d1fb834366ea0a5b6a0605049a88db9225
Oksana_Zvereva <zvereva.oksana@gmail.com>
2020-11-11 18:03:27 +0500

    lesson 1

---
+++ TicketSearch/TicketSearch/AnotherViewController.h
@@ -0,0 +1,16 @@
+//
+//  AnotherViewController.h
+//  TicketSearch
+//
+//  Created by Оксана Зверева on 11.11.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AnotherViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TicketSearch/TicketSearch/AnotherViewController.m
@@ -0,0 +1,32 @@
+//
+//  AnotherViewController.m
+//  TicketSearch
+//
+//  Created by Оксана Зверева on 11.11.2020.
+//
+
+#import "AnotherViewController.h"
+
+@interface AnotherViewController ()
+
+@end
+
+@implementation AnotherViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    [self.view setBackgroundColor:[UIColor systemBlueColor]];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
